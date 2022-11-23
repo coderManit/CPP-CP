608e951c865636dcf08957bd235e83a57a2e4717
coderManit <113006341+coderManit@users.noreply.github.com>
2022-11-23 08:23:32 +0530

    Add files via upload

---
+++ template.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+#define llint long long int
+#define sz(x) (llint)(x).size()
+#define all(v) v.begin(), v.end()
+#define forEach(seq) for(auto i: seq)
+#define gout(i) cout << "Case #" << i << ": ";
+
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+
+
