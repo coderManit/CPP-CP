60d4478e6cfba3bfffea05d0651c62ed1c9aea8c
coderManit <113006341+coderManit@users.noreply.github.com>
2023-01-09 11:53:25 +0530

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,9 +1,14 @@
+/*  
+    Template file: All the new things I learn would be added here
+    Git repo (https://github.com/coderManit/CPP-CP) will be updated accordingly
+*/
 #include<bits/stdc++.h>
 
 #define llint long long int
 #define sz(x) (llint)(x).size()
 #define all(v) v.begin(), v.end()
 #define forEach(seq) for(auto i: seq)
+#define _ceil(x, y)  (x + y - 1)/y
 #define gout(i) cout << "Case #" << i << ": ";
 
 using namespace std;
